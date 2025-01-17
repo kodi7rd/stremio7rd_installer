8cd6540835a7cfdbea5345d9eab88de7dd7d85c0
kodi7rd <KodiRealDebridIsrael@github.com>
2025-01-17 14:40:09 +0200

    Bump build version to 2.4.0

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 // Build version
-const BUILD_VERSION = "2.3.0";
+const BUILD_VERSION = "2.4.0";
 // API
 const STREMIO_API_BASE_URL = "https://api.strem.io/api";
 const STREMIO_API_LOGIN_URL = `${STREMIO_API_BASE_URL}/login`;
